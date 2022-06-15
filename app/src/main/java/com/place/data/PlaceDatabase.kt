921f70137917e2d368a015ec79ebf71c05c40118
robertokepp <54158260+Verokeyty@users.noreply.github.com>
2022-06-15 03:03:26 -0400

    base to vero branch

---
+++ app/src/main/java/com/place/data/PlaceDatabase.kt
@@ -0,0 +1,41 @@
+
+package com.place.data
+
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.place.model.Place
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+
+@Database(entities=[Place::class], version = 1, exportSchema = false)
+
+abstract class PlaceDatabase : RoomDatabase(){
+    abstract fun placeDao() : PlaceDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: PlaceDatabase? = null
+
+
+
+
+        @OptIn(InternalCoroutinesApi::class)
+        fun getDatabase(context: android.content.Context): PlaceDatabase{
+            val tempInstance = INSTANCE
+            if (tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    PlaceDatabase :: class.java,
+                    "place_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/place/ui/place/PlaceFragment.kt
@@ -0,0 +1,34 @@
+package com.place.ui.place
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.place.databinding.FragmentPlaceBinding
+import com.place.ui.viewmodel.PlaceViewModel
+
+class PlaceFragment : Fragment() {
+
+    private lateinit var placeViewModel: PlaceViewModel
+
+    private var _binding: FragmentPlaceBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        placeViewModel = ViewModelProvider(this).get(PlaceViewModel::class.java)
+        _binding = FragmentPlaceBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
